3f2b67904d2c84aac03306677da7f9f96dbb089f
sk035018 <sk035018@users.noreply.github.com>
2020-10-02 20:45:40 +0530

    Final Submit of Functions with Filters

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-function isOdd(num) {
-  return num % 2 !== 0;
+// Ques 1.
+function arrayOdd(arr) {
+  return arr.filter(el => el % 2 !== 0);
+}
+
+// Ques 2.
+function arrayEven(arr) {
+  return arr.filter(isEven);
+}
+
+// Ques 3.
+function arrayPrime(arr) {
+  return arr.filter(isPrime);
+}
+
+// Ques 4.
+function arrayPerfectSquares(arr) {
+  return arr.filter(isPerfectSquare);
+}
+
+// Ques 5.
+function arrayPerfectCubes(arr) {
+  return arr.filter(isPerfectCube);
+}
+
+// Ques 6.
+function arrayArmstrong(arr) {
+  return arr.filter(isArmstrong);
+}
+
+// Ques 7.
+function arrayPalindrome(arr) {
+  return arr.filter(isPalindrome);
+}
+
+// Ques 8.
+function arrayEvenSqure(arr) {
+  return arr.filter(isEven);
+}
+
+// Ques 9.
+function arrayPositive(arr) {
+  return arr.filter(el => el >= 0);
+}
+
+// Ques 10.
+function arrayFactorOf3Or5(arr) {
+  return arr.filter(isThreeOrFiveFactor);
+}
+
+function isEven(num) {
+  return num % 2 === 0;
 }
 
 function isPrime(num) {
